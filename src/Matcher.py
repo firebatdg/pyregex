adf3359e269e0d4470fe778099ea5a659823f473
albamagallanes <albamagallanes@gmail.com>
2012-11-19 20:28:10 -0600

    <3 Matcher

---
+++ src/Matcher.py
@@ -0,0 +1,44 @@
+from State import *
+from Link import *
+
+class Matcher:
+
+    def __init__(self, initState, word):
+        self.initState = initState
+        self.activeNodes = []
+        self.word = word
+        self.currentChar = None
+
+    def searchMatch(self, word):
+        setActiveNode(self.initState)
+        while len(word) != 0:
+            self.currentChar = word[0]
+            word = word[1:]
+            getPossiblePaths()
+            if len(self.activeNodes) == 0:
+                return False
+        if isAcceptingSatate:
+            return true
+        else:
+            return false
+        
+    def getPossiblePaths (self):
+        tempActiveNodes = self.activeNodes
+        self.activeNodes = []
+         for state in tempActiveNodes:
+            for link in state.connections:
+                if link.value == self.currentChar or link.value=="":
+                    setActiveNode(link.state)
+
+    def isAcceptingState(self):
+        for state in self.activeNodes:
+            if state.accepting:
+                return true
+        return false
+
+    def setActiveNode(state):
+        if state not in self.activeNodes:
+            self.activeNodes.append(state)
+
+    
+        
